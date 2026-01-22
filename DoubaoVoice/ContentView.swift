64d4023fe413ae04981ef985d17abdac19e60b14
bmpixel <pixelwenbo@gmail.com>
2026-01-22 09:45:36 +0800

    Initial Commit

---
+++ DoubaoVoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoubaoVoice
+//
+//  Created by 潘文博 on 2026/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoubaoVoice/DoubaoVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubaoVoiceApp.swift
+//  DoubaoVoice
+//
+//  Created by 潘文博 on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DoubaoVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
